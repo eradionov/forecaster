52cb6c36070269f0f8b75e65651307ffd00eb66f
Yauheni Radzivonau <zhenyaradi@gmail.com>
2021-06-19 18:21:42 +0300

    Setup docker, symfony, phpstan, php-cs-fixer, dependencies

---
+++ weather_forecast/.php-cs-fixer.dist.php
@@ -0,0 +1,25 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__.'/src')
+    ->in(__DIR__.'/tests')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        '@Symfony' => true,
+        'ordered_imports' => true,
+        'strict_param' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_superfluous_phpdoc_tags' => false,
+        'phpdoc_annotation_without_dot' => false,
+        'phpdoc_types_order' => false,
+        'phpdoc_summary' => false,
+        'phpdoc_to_comment' => false,
+        'phpdoc_align' => ['align' => 'left'],
+        'yoda_style' => false,
+    ])
+    ->setFinder($finder)
+    ;
