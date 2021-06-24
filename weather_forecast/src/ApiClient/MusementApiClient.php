65c88e801c215811a90ee32c4812a8bc8b8c82ea
Yauheni Radzivonau <zhenyaradi@gmail.com>
2021-06-24 14:33:50 +0300

    TASK-11: Refactor code

--- weather_forecast/src/ApiClient/MusementApiClient.php
+++ weather_forecast/src/ApiClient/MusementApiClient.php
@@ -6,21 +6,10 @@ namespace App\ApiClient;
 
 use App\ApiClient\Interfaces\MusementApiInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MusementApiClient extends BaseApiClient implements MusementApiInterface
 {
     private const CITIES_ENDPOINT = '/api/v3/cities';
-
-    /**
-     * @param SerializerInterface $serializer
-     * @param HttpClientInterface $musementCityClient
-     */
-    public function __construct(SerializerInterface $serializer, HttpClientInterface $musementCityClient)
-    {
-        parent::__construct($serializer, $musementCityClient);
-    }
 
     /**
      * {@inheritdoc}
